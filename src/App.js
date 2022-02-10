28b6150df59540a7b03cdeb004a2c49685751ace
Ryochi <ryo1.t.zubu@gmail.com>
2022-02-10 22:11:32 +1000

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
-import Navbar from './components/Navbar'
-
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SingleCocktail from "./pages/SingleCocktail"
+import Error from "./pages/Error"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Switch>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route path='/about'>
-              <About />
-            </Route>
-            <Route path='/cocktail/:id'>
-              <SingleCocktail />
-            </Route>
-            <Route path='*'>
-              <Error />
-            </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cocktail/:id" component={SingleCocktail} />
+        <Route path="*" component={Error} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return(
-      <div>
-          <h2>About page</h2>
-      </div>
+      <section className='section about-section'>
+          <h1 className='section-title'>about us</h1>
+          <p>
+            Culpa eu dolore irure amet sint do enim exercitation minim culpa dolor aute eiusmod. Aute excepteur aute deserunt ipsum reprehenderit. Do occaecat aute commodo aliquip. Laborum cupidatat sint mollit nostrud ex qui. Eiusmod quis id nulla eiusmod est laborum laborum commodo nostrud nostrud aliquip ullamco veniam. Aliquip ipsum enim consequat exercitation ad minim nisi nisi eu anim sint id. Minim anim ex labore amet.
+
+            Duis ea incididunt enim ad velit pariatur. Pariatur labore nisi reprehenderit voluptate. Cupidatat veniam dolor duis eiusmod sit sit quis. Sint sit cillum in esse non aute cupidatat sit est duis.
+          </p>
+      </section>
   )
 }
 
